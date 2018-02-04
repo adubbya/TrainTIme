3991053f92f50f15453e4cc73918aaf311942580
adubbya <aaron.mitchell@austin.utexas.edu>
2018-02-04 15:23:39 -0600

    form submit to firebase, testing moment.js, fixed some bootstrap increase commit-freq

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,49 +4,86 @@
 - DONE test track active visitors
 - DONE add moment.js
 - sooo much left:
--- firebase recieves data from user input via form
+-- DONE firebase recieves data from user input via form
 -- firebase output to table DOM
--- moment.js logic, buncha stuff 
+-- moment.js logic, still needs pseudocode 
 -- 
 */
 
 
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyAqGAqMPaVeuJxpfxT4fMj66eRPjHra_Mw",
     authDomain: "traintime-85fd2.firebaseapp.com",
     databaseURL: "https://traintime-85fd2.firebaseio.com",
     projectId: "traintime-85fd2",
     storageBucket: "traintime-85fd2.appspot.com",
     messagingSenderId: "1022210536183"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  // Create a variable to reference the database
-  var database = firebase.database();
 
-  // directory of connection and state change
-  var connectionsRef = database.ref("/connections");
-  var connectedRef = database.ref(".info/connected");
+// Firebase directories
+// top-level database ref
+var database = firebase.database();
+// /Trains ref
+var trainsRef = database.ref("/Trains");
+// /connections and state change ref
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
 
-  // Add/Remove user on based on state change
-  connectedRef.on("value", function(snap) {
+// Initial Variables, figure out if i need these at some point
+var startTrainName = "";
+var startTrainDestination = "";
+var startTrainFreq = 5;
+var startArrivalTime= "12:00";
+var startMinAway="";
+
+// Add/Remove user on based on state change
+connectedRef.on("value", function (snap) {
 
     if (snap.val()) {
 
-      var con = connectionsRef.push(true);
+        var con = connectionsRef.push(true);
 
-      con.onDisconnect().remove();
+        con.onDisconnect().remove();
     }
-  });
-  
-  // console log #of connections, eventually html stuff
-  connectionsRef.on("value", function(snap) {
+});
+
+// console log #of connections,
+connectionsRef.on("value", function (snap) {
     console.log(snap.numChildren());
-  });
+});
+
+
+// DONE Add Train Data with submit button
+$("#submit-train").on("click", function (event) {
+    event.preventDefault();
+
+    // DONE Train data from user input
+    var trainName = $("#train-name").val().trim();
+    var trainDest = $("#train-destination").val().trim();
+    // horrible mutant, but atleasts its moment js
+    var trainTime = moment($("#train-time").val().trim(), "hh:mm").format();
+    var trainFreq = $("#train-frequency").val().trim();
+
+    // DONE new train temp object
+    var newTrain = {
+        name: trainName,
+        dest: trainDest,
+        time: trainTime,
+        freq: trainFreq
+    };
+
+    trainsRef.push(newTrain);
+    // object to database yay!
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.time);
+    console.log(newTrain.freq);
+
+});
   
-  // Initial Variables, figure those out at some point
-  var trainName = "";
-  var trainDestination = "";
-  var trainFreq = "";
+  
+
